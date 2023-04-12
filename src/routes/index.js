990bca90c50e8d654c1df35cf5c9996efa88620c
Domenic Betters <108141041+domenicbetters@users.noreply.github.com>
2023-04-12 15:46:39 -0400

    added several new sets of cards plus godzilla and doom

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ import EC2Grid from "@/views/EC2Grid.vue"
 import EC2Flash from "@/views/EC2Flash.vue"
 import batchflash from "@/views/BatchFlash.vue"
 import BeanstalkFlash from "@/views/BeanstalkFlash.vue"
+import LambdaFlash from "@/views/LambdaFlash.vue"
+import ServerlessARFlash from "@/views/ServerlessARFlash.vue"
+import FargateFlash from "@/views/FargateFlash.vue"
 import { createRouter, createWebHistory } from "vue-router";
 
 
@@ -19,12 +22,12 @@ const routes = [
       component: MainMenu
     },
     {
-        path: "/awszilla/EC2Grid",
+        path: "/awszilla/ec2grid",
         name: "EC2Grid",
         component: EC2Grid
       },
       {
-        path: "/awszilla/EC2Flash",
+        path: "/awszilla/ec2flash",
         name: "EC2Flash",
         component: EC2Flash
       },
@@ -37,6 +40,21 @@ const routes = [
         path: "/awszilla/beanstalkflash",
         name: "BeanstalkFlash",
         component: BeanstalkFlash
+      },
+      {
+        path: "/awszilla/lambdaflash",
+        name: "LambdaFlash",
+        component: LambdaFlash
+      },
+      {
+        path: "/awszilla/serverlessarflash",
+        name: "ServerlessARFlash",
+        component: ServerlessARFlash
+      },
+      {
+        path: "/awszilla/fargateflash",
+        name: "FargateFlash",
+        component: FargateFlash
       }
 
 
--- src/views/MainMenu.vue
+++ src/views/MainMenu.vue
@@ -1,11 +1,18 @@
 <template>
+  <div class = "bady" >
     <h1>AWSZILLA</h1>
     <div>
      <router-link :to="{name: 'EC2Grid'}"> <button>EC2 Instance Grid Flash Cards</button></router-link>
      <router-link :to="{name: 'EC2Flash'}"> <button>EC2 Flash Cards</button></router-link>
      <router-link :to="{name: 'batchflash'}"> <button>AWS Batch Flash Cards</button></router-link>
      <router-link :to="{name: 'BeanstalkFlash'}"> <button>AWS Beanstalk Flash Cards</button></router-link>
+     <a href="https://dos.zone/player/?bundleUrl=https%3A%2F%2Fcdn.dos.zone%2Fcustom%2Fdos%2Fdoom.jsdos?anonymous=1%27)"><button>DOOM</button></a> 
+     <router-link :to="{name: 'LambdaFlash'}"> <button>AWS Lambda Flash Cards</button></router-link>
+     <router-link :to="{name: 'ServerlessARFlash'}"> <button>AWS Serverless Application repo Flash Cards</button></router-link>
+     <router-link :to="{name: 'FargateFlash'}"> <button>AWS Fargate Flash Cards</button></router-link>
     </div>
+    <div class="imgbox"><img src="@/assets/godzilla.jpg" /></div>
+  </div>
   </template>
 
 
@@ -19,5 +26,22 @@
 <style>
 button {
   margin:10px;
+}
+
+.imgbox {
+  width: 90vw;
+  box-shadow: 0 0 30px 20px rgba(142, 142, 252, 0.536);
+  border: 2px solid white;
+  margin-top: 50px;
+}
+img {
+  width:100%;
+  height: 100%;
+  object-fit: cover;
+}
+.bady {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
